2eb64f26bdfde6808e62d7aee8e0fa663a79963c
arendtio <finn-github-contributions@ubermail.me>
2018-11-15 02:46:28 +0100

    update dependencies

---
+++ module/module-validate-0.9.1.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+src="$1"
+targetHash="$2"
+
+module "moduleChecksum" "https://mdl.sh/module/module-checksum-0.9.0.sh" "cksum-cksum-24661179"
+
+hashFunc="$(echo "$targetHash" | cut -d '-' -f1)"
+srcHash="$(moduleChecksum "$src" "$hashFunc")"
+if [ "$srcHash" != "$targetHash" ]; then
+	return 1
+fi
