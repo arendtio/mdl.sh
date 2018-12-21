e1b17193a65ee5594e1bc4e50266c9256cf0b74d
arendtio <finn-github-contributions@ubermail.me>
2018-12-21 12:17:26 +0100

    add spec for module error

---
+++ error/spec/error.spec.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+implementation="$1"
+directory="$2"
+
+module "errImpl" "$implementation"
+module "error" "https://mdl.sh/error/error-1.0.1.sh" "cksum-1107954660"
+module "assertEqual" "https://mdl.sh/spec-test/assert-equal-0.9.1.sh" "cksum-2022066480"
+
+# file does not exist
+if errImpl "No real Error" >/dev/null 2>&1; then
+	error "TEST: Error does not return a non-zero return code" 1
+fi
+
+# stderr output
+result="$(errImpl "No real error" 2>&1 >/dev/null || true)"
+target="No real error"
+assertEqual "Stderr output" "$result" "$target"
+
+# nothing to stdout
+result="$(errImpl "No real error" 2>/dev/null || true)"
+target=""
+assertEqual "No output to stdout" "$result" "$target"
+
+# custom return value
+errImpl "No real Error" 42 >/dev/null 2>&1 || result="$?" && true
+target="42"
+assertEqual "Custom return code" "$result" "$target"
+
+
