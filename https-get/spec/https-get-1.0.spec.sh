0d0837273165ecaa7948ffe0f2e958b7d0602909
arendtio <finn-github-contributions@ubermail.me>
2019-02-11 22:27:37 +0100

    add spec for module https-get

---
+++ https-get/spec/https-get-1.0.spec.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+implementation="$1"
+directory="$2"
+
+module "httpsGet" "$implementation"
+module "assertEqual" "https://mdl.sh/spec-test/assert-equal-0.9.4.sh" "cksum-566303087"
+
+DEBUG_NAMESPACE="HTTPS_GET_SPEC"
+
+## normal test
+result="$(httpsGet "https://mdl.sh/hello-world/hello-world-1.0.0.sh" | cksum)"
+target="1346054948 31"
+assertEqual "Basic hello-world fetch" "$result" "$target"
