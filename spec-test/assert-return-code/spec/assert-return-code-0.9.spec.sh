2728656b66d5e0acf811cce74debea17ad2361f8
arendtio <finn-github-contributions@ubermail.me>
2019-04-14 14:10:57 +0200

    add module spec-test/assert-return-code with spec

---
+++ spec-test/assert-return-code/spec/assert-return-code-0.9.spec.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+implementation="$1"
+
+module "assertReturnCode" "$implementation"
+module "error" "https://mdl.sh/error/error-1.0.3.sh" "cksum-2734170982"
+module "debug" "https://mdl.sh/debug/debug-0.9.2.sh" "cksum-2374238394"
+module "assertEqual" "https://mdl.sh/spec-test/assert-equal-0.9.4.sh" "cksum-566303087"
+
+# shellcheck disable=SC2034  # The debug module uses this variable
+DEBUG_NAMESPACE="ASSERT_RETURN_CODE_SPEC"
+
+#
+# Error Tests
+#
+## No arguments
+target="64"
+result="0"
+assertReturnCode >/dev/null 2>&1 || result="$?" && true
+assertEqual "No arguments" "$result" "$target"
+
+## Too many arguments
+target="64"
+result="0"
+assertReturnCode "One" "Two" "Three" "Four" >/dev/null 2>&1 || result="$?" && true
+assertEqual "Too many arguments" "$result" "$target"
+
+## Empty first arguments
+target="1"
+result="0"
+assertReturnCode "" "1" "ls">/dev/null 2>&1 || result="$?" && true
+assertEqual "Empty first argument" "$result" "$target"
+
+#
+# Normal Tests
+#
+# just return 0 if rc matches the expected rc is fine
+# expect rc = 0
+target="0"
+result="0"
+assertReturnCode "example descrition" "0" "sh -c 'exit 0;'" >/dev/null 2>&1 || result="$?" && true
+assertEqual "Return code zero if command returns zero as expected" "$result" "$target"
+
+# expect rc = 1
+target="0"
+result="0"
+assertReturnCode "example descrition" "1" "sh -c 'exit 1;'" >/dev/null 2>&1 || result="$?" && true
+assertEqual "Return code zero if command returns zero as expected" "$result" "$target"
+
+# return 1 if expectations are not matched
+# expect rc = 0
+target="1"
+result="0"
+assertReturnCode "example descrition" "0" "sh -c 'exit 1;'" >/dev/null 2>&1 || result="$?" && true
+assertEqual "Return code one if command returns one but should not" "$result" "$target"
+
+# expect rc = 1
+target="1"
+result="0"
+assertReturnCode "example descrition" "1" "sh -c 'exit 0;'" >/dev/null 2>&1 || result="$?" && true
+assertEqual "Return code one if command returns zero but should not" "$result" "$target"
+
+# use a custom function within the cmd
+retOne() {
+	return 1;
+}
+target="0"
+result="0"
+#assertReturnCode "example descrition" "1" "retOne" >/dev/null 2>&1 || result="$?" && true
+assertReturnCode "example descrition" "1" "retOne" || result="$?" && true
+assertEqual "Custom function" "$result" "$target"
+
+
