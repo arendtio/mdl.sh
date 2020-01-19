d751aff6af4a780c0b3aa3e278e9770c8832fc55
arendtio <finn-github-contributions@ubermail.me>
2020-01-19 14:43:57 +0100

    new module-checksum-1.0.0 with input via stdin instead of $1

---
+++ module/checksum/module-checksum-1.0.0.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# check number of arguments
+if [ "$#" -gt 1 ]; then
+	printf 'Invalid number of arguments\n' >&2
+	exit 64
+fi
+
+# optional:
+# command to calculate the hash, examples:
+# cksum (default, POSIX compliant, weak)
+# md5sum (fast)
+# sha256sum (secure)
+hashCmd="${1:-cksum}"
+
+module "error" "https://mdl.sh/error/error-1.0.3.sh" "cksum-2734170982"
+
+if ! command -v "$hashCmd" >/dev/null 2>&1; then
+	error "Checksum command '$hashCmd' is not available. Exiting." 69
+fi
+
+# the content should come from a pipe in order to allow untrimmed whitespace
+# as a parameter it is possible too, but putting the whitespace in a
+# variable is kinda complicated because sub-shells remove it
+
+printf '%s-' "$hashCmd"
+cat - | "$hashCmd" | tr -s '[:blank:]' ' ' | cut -d ' ' -f1
+printf '\n'
---
+++ module/checksum/spec/module-checksum-1.0.spec.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+implementation="$1"
+
+module "moduleChecksum" "$implementation"
+module "error" "https://mdl.sh/error/error-1.0.3.sh" "cksum-2734170982"
+module "assertEqual" "https://mdl.sh/spec-test/assert/equal/assert-equal-0.9.6.sh" "cksum-2671631268"
+module "assertReturnCode" "https://mdl.sh/spec-test/assert/return-code/assert-return-code-0.9.3.sh" "cksum-1471193511"
+
+# shellcheck disable=SC2034  # The debug module uses this variable
+DEBUG_NAMESPACE="MODULE_CHECKSUM_SPEC"
+
+#
+# Test errors
+#
+# omiting all arguments will block (as intended)
+
+# too many arguments
+target="64"
+cmd="moduleChecksum 'one' 'two'"
+assertReturnCode "Too many arguments" "$target" "$cmd"
+
+# not existing hash command
+target="69"
+cmd="printf X | moduleChecksum 'neverEverHash'"
+assertReturnCode "Non-existing hash command" "$target" "$cmd"
+
+#
+# Normal Tests
+#
+# empty content
+result="$(printf '' | moduleChecksum)"
+target="cksum-4294967295"
+assertEqual "Empty content" "$result" "$target"
+
+# just a newline
+result="$(printf '\n' | moduleChecksum)"
+target="cksum-3515105045"
+assertEqual "One newline" "$result" "$target"
+
+# two newlines
+result="$(printf '\n\n' | moduleChecksum)"
+target="cksum-3518178554"
+assertEqual "Two newlines" "$result" "$target"
+
+# two words
+result="$(printf 'Foo Bar' | moduleChecksum)"
+target="cksum-2520678754"
+assertEqual "Two words" "$result" "$target"
+
+# two lines
+result="$(printf 'Foo\nBar\n'| moduleChecksum)"
+target="cksum-214098449"
+assertEqual "Two lines" "$result" "$target"
+
+# custom hash command (function)
+# just add Z as prefix and suffix
+myHash() {
+	printf 'Z%sZ' "$(cat -)"
+}
+result="$(printf 'abc' | moduleChecksum "myHash")"
+target="myHash-ZabcZ"
+assertEqual "Custom Hash function" "$result" "$target"
+
+# First field of the hash
+myHashWords() {
+	printf 'A B C'
+}
+result="$(printf 'abc' | moduleChecksum "myHashWords")"
+target="myHashWords-A"
+assertEqual "Return only first word of hash" "$result" "$target"
+
+# First field of the hash (tabs)
+myHashTabWords() {
+	printf 'A\tB\tC'
+}
+result="$(printf 'abc' | moduleChecksum "myHashTabWords")"
+target="myHashTabWords-A"
+assertEqual "Return only first word of hash (tabs)" "$result" "$target"
